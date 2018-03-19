cb82de807c8b3976004e90bd2e1f9d144d1e7efa
seokjae hong <hsj2334@gmail.com>
2018-03-19 15:50:26 +0900

    viewset설정

--- app/snippets/apis/viewsets.py
+++ app/snippets/apis/viewsets.py
@@ -1,14 +1,22 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 
 from snippets.permission import IsOwnerOrReadOnly
 from ..models import Snippet
 from ..serializers import SnippetSerializer
 
 
-class SnippetViewSet(viewsets.Modelviewset):
+class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
         IsOwnerOrReadOnly,
     )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
